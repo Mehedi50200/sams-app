9fcc7be9620fec922fcda8e6973496dae0c2f0f4
Mehedi50200 <mehedihasansony1@hotmail.com>
2018-09-14 05:13:10 +0800

    May Allah bless me. All Main Process Done!!

--- app/src/main/java/my/unimas/a50200siswa/studentattendancemonitoringsystem/RecyclerViewAdapterCroppedImages.java
+++ app/src/main/java/my/unimas/a50200siswa/studentattendancemonitoringsystem/RecyclerViewAdapterCroppedImages.java
@@ -3,6 +3,8 @@ package my.unimas.a50200siswa.studentattendancemonitoringsystem;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -76,10 +78,24 @@ class RecyclerViewAdapterCroppedImages extends RecyclerView.Adapter<RecyclerView
         holder.StudentMatric.setText(mData.get(position).getStudentMatric());
         holder.StudentStatus.setText(mData.get(position).getAttendanceRecord());
 
+        if(mData.get(position).getAttendanceRecord().equals("Present")){
+            holder.IVattendance.setBackgroundResource(R.drawable.presentdot);
+        }else if(mData.get(position).getAttendanceRecord().equals("Absent")){
+            holder.IVattendance.setBackgroundResource(R.drawable.absentdot);
+        }else if(mData.get(position).getAttendanceRecord().equals("Failed")){
+            holder.IVattendance.setBackgroundResource(R.drawable.faileddot);
+        }
+
         if(mData.get(position).getAttendanceRecord().equals("Failed")){
             holder.LPData.setBackgroundResource(R.drawable.card_gradientpurple);
+        }else if (mData.get(position).getAttendanceRecord().equals("Present")) {
+            holder.LPData.setBackgroundColor(Color.parseColor("#ffffff"));
+        }else if (mData.get(position).getAttendanceRecord().equals("Absent")) {
+            holder.LPData.setBackgroundColor(Color.parseColor("#ffffff"));
         }
-        if(mData.get(position).getStudentMatric().length()<5){
+
+
+        if(mData.get(position).getStudentMatric().length()<5 || mData.get(position).getStudentMatric().length()>5){
             holder.LPData.setBackgroundResource(R.drawable.card_gradientblue);
         }
 
@@ -106,7 +122,31 @@ class RecyclerViewAdapterCroppedImages extends RecyclerView.Adapter<RecyclerView
             }
         });
 
-        holder.StudentStatus.setOnClickListener(new View.OnClickListener() {
+
+        holder.btnItemRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.LRemoveItem.setVisibility(View.VISIBLE);
+            }
+        });
+
+        holder.btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeItem(position);
+                holder.LRemoveItem.setVisibility(View.GONE);
+            }
+        });
+
+        holder.btnNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.LRemoveItem.setVisibility(View.GONE);
+            }
+        });
+
+
+        holder.IVattendance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -141,6 +181,14 @@ class RecyclerViewAdapterCroppedImages extends RecyclerView.Adapter<RecyclerView
         notifyDataSetChanged();
     }
 
+    public void removeItem(int position) {
+        mData.remove(position);
+        // notify the item removed by position
+        // to perform recycler view delete animations
+        // NOTE: don't call notifyDataSetChanged()
+        notifyItemRemoved(position);
+    }
+
     @Override
     public int getItemCount() {
         return mData.size();
@@ -149,24 +197,38 @@ class RecyclerViewAdapterCroppedImages extends RecyclerView.Adapter<RecyclerView
 
     class CroppedimageViewHolder extends RecyclerView.ViewHolder {
         TextView StudentNo, StudentMatric, StudentStatus;
-        ImageView CroppedImage;
-        LinearLayout LPData;
+        ImageView CroppedImage, IVattendance;
+        LinearLayout LPData, LRemoveItem;
         EditText ETCorrectStudentMatric;
-        Button btnSaveStudentMatric, btnCancel;
+        Button btnSaveStudentMatric, btnCancel, btnItemRemove, btnYes,btnNo;
         LinearLayout lEditStudentMatric;
+        CardView CardViewCroppedImage;
 
         private CroppedimageViewHolder(View itemView) {
             super(itemView);
+            LPData = itemView.findViewById(R.id.lpdata);
+            lEditStudentMatric = itemView.findViewById(R.id.leditstudentmatric);
+            LRemoveItem = itemView.findViewById(R.id.lremoveitem);
+
+            IVattendance =itemView.findViewById(R.id.ivattendance);
+
+            btnSaveStudentMatric =itemView.findViewById(R.id.btnsavestudentmatric);
+            btnCancel = itemView.findViewById(R.id.btncanceleditingstudentmatric);
+
+            btnItemRemove =itemView.findViewById(R.id.btnitemremove);
+            btnYes = itemView.findViewById(R.id.btnritemYes);
+            btnNo =itemView.findViewById(R.id.btnritemNo);
+
+
             StudentNo = itemView.findViewById(R.id.tvtxtprocessstudentno);
             CroppedImage = itemView.findViewById(R.id.ivcroppedimage);
             StudentMatric = itemView.findViewById(R.id.tvtxtprocessstudentid);
             StudentStatus = itemView.findViewById(R.id.tvtxtprocessstudentstatus);
-            LPData = itemView.findViewById(R.id.lpdata);
-            ETCorrectStudentMatric = itemView.findViewById(R.id.etcstudentid);
-            btnSaveStudentMatric =itemView.findViewById(R.id.btnsavestudentmatric);
-            lEditStudentMatric = itemView.findViewById(R.id.leditstudentmatric);
-            btnCancel = itemView.findViewById(R.id.btncanceleditingstudentmatric);
 
+            ETCorrectStudentMatric = itemView.findViewById(R.id.etcstudentid);
+
+
+            CardViewCroppedImage = itemView.findViewById(R.id.cardviewcroppedimage);
         }
     }
 
